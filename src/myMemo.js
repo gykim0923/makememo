41c8239d92ec11d8bae0e9fcd0298375da6e0c20
김가영 <83538598+gykim0923@users.noreply.github.com>
2021-11-08 00:18:03 +0900

    초기화 성공

--- src/myMemo.js
+++ src/myMemo.js
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
 
 function main() {
-  const [text, setText] = useState("");
-
+  const [text1, setTitle] = useState("");
+  const [text2, setContex] = useState("");
   const onChangeTitle = (e) => {
-    setText(e.target.value);
+    setTitle(e.target.value);
   };
   const onChangeContex = (e) => {
-    setText(e.target.value);
+    setContex(e.target.value);
   };
 
   const onReset = () => {
-    setText("");
+    setTitle("");
+    setContex("");
   };
   return (
     <div>
       <h2>메모장</h2>
       <h3>제목</h3>
-      <input onChange={onChangeTitle} value={text} />
+      <input onChange={onChangeTitle} value={text1} />
       <h3>내용</h3>
-      <input onChange={onChangeContex} value={text} />
+      <input onChange={onChangeContex} value={text2} />
 
       <button>저장</button>
       <button onClick={onReset}>초기화</button>
